1a73e0fb7a25f3cba5af4372f49e5dbcb6d043e4
Xei201 <xei201@yandex.ru>
2023-05-12 11:48:16 +0300

    correct HTML

--- flower/flower/urls.py
+++ flower/flower/urls.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
 from django.urls import path, include
-
-# только на период разработки
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -11,8 +9,6 @@ urlpatterns = [
     path("accounts/", include("django.contrib.auth.urls")),
 ]
 
-
-# только на период разработки
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 if settings.DEBUG:
